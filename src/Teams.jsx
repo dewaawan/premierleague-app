3c60c8d6846f2d7798835857982c2f0752ad5783
dewaawan <idewa02setiawan@gmail.com>
2021-03-06 10:44:02 +0700

    membuat halaman teams

--- src/Teams.jsx
+++ src/Teams.jsx
@@ -4,7 +4,29 @@ export class Teams extends Component {
     render() {
         return (
             <div>
-                Teams
+                <div className="bg-gradient-to-r from-purple-700 to-purple-800 mx-0">
+                    <p align="right" className=" text-5xl font-extrabold text-gray-50 p-20">Teams</p> 
+                </div>
+                <div className="container mx-auto grid grid-cols-5 gap-4 p-4 my-10 bg-white rounded-md">
+                    <div align="center" className="border rounded-sm border-blue-900 py-10">
+                        <img src="https://resources.premierleague.com/premierleague/badges/25/t6@x2.png" alt="tottenham"/>Tottenham
+                    </div>
+                    <div align="center" className="border rounded-sm border-blue-900 py-10">
+                        <img src="https://resources.premierleague.com/premierleague/badges/25/t6@x2.png" alt="tottenham"/>Tottenham
+                    </div>
+                    <div align="center" className="border rounded-sm border-blue-900 py-10">
+                        <img src="https://resources.premierleague.com/premierleague/badges/25/t6@x2.png" alt="tottenham"/>Tottenham
+                    </div>
+                    <div align="center" className="border rounded-sm border-blue-900 py-10">
+                        <img src="https://resources.premierleague.com/premierleague/badges/25/t6@x2.png" alt="tottenham"/>Tottenham
+                    </div>
+                    <div align="center" className="border rounded-sm border-blue-900 py-10">
+                        <img src="https://resources.premierleague.com/premierleague/badges/25/t6@x2.png" alt="tottenham"/>Tottenham
+                    </div>
+                    <div align="center" className="border rounded-sm border-blue-900 py-10">
+                        <img src="https://resources.premierleague.com/premierleague/badges/25/t6@x2.png" alt="tottenham"/>Tottenham
+                    </div>
+                </div>
             </div>
         )
     }
